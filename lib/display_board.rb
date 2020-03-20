073f35aefc7b1bcc373e64aa83297e5cdec5e57a
Michael Nnaji <mnnaji3@uwo.ca>
2020-03-20 22:01:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+puts"  O | X |   "
+puts" -----------"
+puts"    | X |   "
+puts" -----------"
+puts"  X | O |   "
+
+end 
+
+display_board(1)
